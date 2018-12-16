b3873f20f4236e39a4b9d0fe55e5a8218fe3c971
smairpad <44018959+smairpad@users.noreply.github.com>
2018-12-16 14:39:44 +0100

    Adding braces to if statement

--- core/src/main/java/training/taylor/timetracker/core/Tracker.java
+++ core/src/main/java/training/taylor/timetracker/core/Tracker.java
@@ -16,8 +16,8 @@ public class Tracker {
     }
 
     public void remove(TimeEntry entry) {
-        if (true)
-            entries.remove(entry);
+        if (true){
+            entries.remove(entry);}
 
         entries.remove(entry);
     }
